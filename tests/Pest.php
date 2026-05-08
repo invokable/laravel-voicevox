5e338e567a75a72814aded50479d80d162356898
kawax <1502086+kawax@users.noreply.github.com>
2026-05-08 13:43:42 +0000

    fix code style

--- tests/Pest.php
+++ tests/Pest.php
@@ -16,8 +16,8 @@ use Tests\TestCase;
 |
 */
 
- pest()->extend(TestCase::class)
- // ->use(RefreshDatabase::class)
+pest()->extend(TestCase::class)
+// ->use(RefreshDatabase::class)
     ->in('Feature', 'Unit');
 
 /*
