0821f51ca2327aaee8f270b19772762954c7e41f
kawax <kawaxbiz@gmail.com>
2026-05-12 07:56:16 +0900

    Update Note.php

--- src/Song/Note.php
+++ src/Song/Note.php
@@ -14,6 +14,7 @@ readonly class Note implements Arrayable
      * @param  int  $length  音符のフレーム長。秒数に93.75をかけ、端数を調整して整数にしたもの。例として125BPM (Beats Per Minute)における一拍は: 93.75[フレーム/秒] / (125[拍/分] / 60[秒/分]) = 45[フレーム/拍]
      * @param  string  $lyric  歌詞。音符の場合、一つのモーラを表すひらがな/カタカナ（例: "ド", "ファ"）。休符の場合、空文字列。
      * @param  null|int  $key  音階。音符の場合、MIDIのnote number（例: C4なら 60）。休符の場合、null
+     * @param  null|string  $id  FrameAudioQueryを生成するときにFramePhoneme.note_idにコピーされる。歌唱音声には影響しない。
      */
     public function __construct(
         public int $length,
