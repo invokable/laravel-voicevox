1473b168d189360dcb8e5ca0b39ae3a426a21171
kawax <kawaxbiz@gmail.com>
2026-05-15 19:34:50 +0900

    Update EngineRoutesTest.php

--- tests/Feature/Engine/EngineRoutesTest.php
+++ tests/Feature/Engine/EngineRoutesTest.php
@@ -2,12 +2,7 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Facades\Http;
 use Revolution\Voicevox\Voicevox;
-
-beforeEach(function () {
-    Http::preventStrayRequests();
-});
 
 test('engine audio_query endpoint returns json', function () {
     Voicevox::expects('baseUrl->audioQuery')->andReturn(['speedScale' => 1.0]);
@@ -19,9 +14,7 @@ test('engine audio_query endpoint returns json', function () {
 });
 
 test('engine synthesis endpoint returns wav', function () {
-    Http::fake([
-        'http://127.0.0.1:50021/synthesis*' => Http::response('wav_binary_data', 200, ['Content-Type' => 'audio/wav']),
-    ]);
+    Voicevox::expects('baseUrl->synthesis')->andReturn('wav_binary_data');
 
     $response = $this->postJson('/synthesis?speaker=1', ['speedScale' => 1.0]);
 
@@ -30,9 +23,7 @@ test('engine synthesis endpoint returns wav', function () {
 });
 
 test('engine speakers endpoint returns array', function () {
-    Http::fake([
-        'http://127.0.0.1:50021/speakers*' => Http::response([['name' => 'ずんだもん']]),
-    ]);
+    Voicevox::expects('baseUrl->speakers')->andReturn([['name' => 'ずんだもん']]);
 
     $response = $this->getJson('/speakers');
 
@@ -41,9 +32,7 @@ test('engine speakers endpoint returns array', function () {
 });
 
 test('engine speaker_info endpoint returns info', function () {
-    Http::fake([
-        'http://127.0.0.1:50021/speaker_info*' => Http::response(['policy' => 'test']),
-    ]);
+    Voicevox::expects('baseUrl->speaker')->andReturn(['policy' => 'test']);
 
     $response = $this->getJson('/speaker_info?speaker_uuid=388f246b-8c41-4ac1-8e2d-5d79f3ff56d9');
 
@@ -52,9 +41,7 @@ test('engine speaker_info endpoint returns info', function () {
 });
 
 test('engine singers endpoint returns array', function () {
-    Http::fake([
-        'http://127.0.0.1:50021/singers*' => Http::response([['name' => 'ずんだもん']]),
-    ]);
+    Voicevox::expects('baseUrl->singers')->andReturn([['name' => 'ずんだもん']]);
 
     $response = $this->getJson('/singers');
 
@@ -62,9 +49,7 @@ test('engine singers endpoint returns array', function () {
 });
 
 test('engine singer_info endpoint returns info', function () {
-    Http::fake([
-        'http://127.0.0.1:50021/singer_info*' => Http::response(['policy' => 'test']),
-    ]);
+    Voicevox::expects('baseUrl->singer')->andReturn(['policy' => 'test']);
 
     $response = $this->getJson('/singer_info?speaker_uuid=388f246b-8c41-4ac1-8e2d-5d79f3ff56d9');
 
@@ -72,9 +57,7 @@ test('engine singer_info endpoint returns info', function () {
 });
 
 test('engine version endpoint returns version', function () {
-    Http::fake([
-        'http://127.0.0.1:50021/version' => Http::response('"0.20.0"'),
-    ]);
+    Voicevox::expects('baseUrl->version')->andReturn('0.20.0');
 
     $response = $this->getJson('/version');
 
