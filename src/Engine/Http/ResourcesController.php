ec0674cbd594f4b6a4a14c969c210b4fe202d081
kawax <kawaxbiz@gmail.com>
2026-05-16 16:55:39 +0900

    Update ResourcesController.php

--- src/Engine/Http/ResourcesController.php
+++ src/Engine/Http/ResourcesController.php
@@ -5,16 +5,25 @@ declare(strict_types=1);
 namespace Revolution\Voicevox\Engine\Http;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Revolution\Voicevox\Engine\ResourceManager;
 use Revolution\Voicevox\Engine\ResourceManagerError;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class ResourcesController
 {
+    public function __construct(
+        protected ?string $characterInfoPath = null,
+    ) {}
+
     public function __invoke(Request $request, string $hash): BinaryFileResponse
     {
         $rm = new ResourceManager(createFilemapIfNotExist: true);
-        $rm->registerDir(dirname(__DIR__, 3).'/resources/character_info');
+        $dir = $this->characterInfoPath ?? dirname(__DIR__, 3).'/resources/character_info';
+
+        if (File::isDirectory($dir)) {
+            $rm->registerDir($dir);
+        }
 
         try {
             return response()->file($rm->resourcePath($hash));
