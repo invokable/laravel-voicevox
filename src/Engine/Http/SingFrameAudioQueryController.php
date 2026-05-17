55ae3d8e39a4b92bdbe80d0edf504254ed5958ff
kawax <kawaxbiz@gmail.com>
2026-05-18 06:43:28 +0900

    Update SingFrameAudioQueryController.php

--- src/Engine/Http/SingFrameAudioQueryController.php
+++ src/Engine/Http/SingFrameAudioQueryController.php
@@ -6,6 +6,7 @@ namespace Revolution\Voicevox\Engine\Http;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Revolution\Voicevox\Synthesizer;
 use Revolution\Voicevox\Voicevox;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
@@ -16,6 +17,17 @@ class SingFrameAudioQueryController
     {
         $score = $request->json()->all();
         $speaker = $request->integer('speaker', 6000);
+
+        try {
+            $frame_audio_query = Synthesizer::createSingFrameAudioQuery(json_encode($score), $speaker);
+
+            return response()->json(
+                json_decode($frame_audio_query),
+                options: JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE,
+            );
+        } catch (Throwable) {
+            // Fall back to Voicevox client if native core is unavailable
+        }
 
         try {
             return response()->json(
