d399d31057e4ae30daf8744cf42b76e894a1f13c
kawax <kawaxbiz@gmail.com>
2026-05-17 19:02:11 +0900

    Update ValidateKanaController.php

--- src/Engine/Http/ValidateKanaController.php
+++ src/Engine/Http/ValidateKanaController.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Revolution\Voicevox\Engine\Http;
 
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Revolution\Voicevox\Support\KanaConverter;
-use Revolution\Voicevox\Voicevox;
-use Symfony\Component\HttpFoundation\Response;
-use Throwable;
 
 class ValidateKanaController
 {
@@ -21,7 +19,7 @@ class ValidateKanaController
             KanaConverter::parse($text);
 
             return response()->json(json_encode(true));
-        } catch (Throwable) {
+        } catch (Exception $e) {
 
         }
     }
