5776a53faf84b508abe8f2d8e7782cc9c1ddabf8
kawax <kawaxbiz@gmail.com>
2026-05-30 09:04:34 +0900

    Update Unsupported.php

--- src/Client/Concerns/Unsupported.php
+++ src/Client/Concerns/Unsupported.php
@@ -14,29 +14,6 @@ use Revolution\Voicevox\VoicevoxResponse;
 trait Unsupported
 {
     /**
-     * Synthesize multiple audio queries in batch.
-     *
-     * @param  array<array>  $audioQueries
-     * @return VoicevoxResponse zip file containing the synthesized audio files
-     *
-     * @throws RequestException
-     * @throws ConnectionException
-     */
-    public function multiSynthesis(array $audioQueries, int|string $id, bool $interrogativeUpspeak = false): VoicevoxResponse
-    {
-        $body = $this->http()->withQueryParameters(array_filter([
-            'speaker' => $id,
-            'enable_interrogative_upspeak' => $interrogativeUpspeak,
-            'core_version' => config('voicevox.client.core_version'),
-        ], fn ($v) => ! is_null($v)))
-            ->post('multi_synthesis', $audioQueries)
-            ->throw()
-            ->body();
-
-        return new VoicevoxResponse($body);
-    }
-
-    /**
      * Supported devices info.
      */
     public function supportedDevices(): array
